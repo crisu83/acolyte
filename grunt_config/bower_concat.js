03be28290662e2d1152ea366b9f221aae6e06053
Christoffer Niska <christofferniska@gmail.com>
2015-01-19 23:53:58 -0800

    Update bower_concat.js

--- grunt_config/bower_concat.js
+++ grunt_config/bower_concat.js
@@ -4,6 +4,9 @@ module.exports = function (grunt, options) {
       dest: '<%= buildRoot %>/js/vendor.js',
       dependencies: {
         'angular': 'jquery'
+      },
+      bowerOptions: {
+        relative: false
       }
     }
   };
